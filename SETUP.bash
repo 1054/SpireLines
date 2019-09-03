7dd86c0e4479e467398af6d1df7eed349a4dfcf9
1054 <liudz1054@gmail.com>
2019-09-03 15:43:54 +0200

    Updates

--- SETUP.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# 
-
-
-Crab_BIN_SETUP_SCRIPT=$(dirname "${BASH_SOURCE[0]}")/Software/bin_setup.bash
-
-source "$Crab_BIN_SETUP_SCRIPT" -check FTS_tool_convolve_fits_image FTS_tool_draw_pointing_pattern
-
-
